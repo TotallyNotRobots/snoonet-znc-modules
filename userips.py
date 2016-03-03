b6dfdd062b8ee4029c85ef648f6729221e550fca
evansnoonet <evan.snoonet@gmail.com>
2016-03-02 21:41:45 -0500

    Update and rename LICENSE.md to userips.py

---
+++ userips.py
@@ -0,0 +1,32 @@
+import znc
+
+class userips(znc.Module):
+    module_types = [znc.CModInfo.GlobalModule]
+    description = "Shows IP addresses of connected users"
+
+    def WebRequiresAdmin(self):
+        return True
+
+    def GetWebMenuTitle(self):
+        return "User IPs"
+
+    def OnWebRequest(self, sock, page, tmpl):
+
+        users = znc.CZNC.Get().GetUserMap()
+
+        for user in users.items():
+            row = tmpl.AddRow("UserLoop")
+            row["User"] = user[0]
+
+            net = user[1].FindNetwork("freenode")
+            try:
+                clients = net.GetClients()
+                out = ''
+                for client in clients:
+                    out += client.GetRemoteIP() + ' '
+
+                row["IP"] = out
+            except:
+                row["IP"] = "Network Not Found"
+
+        return True
