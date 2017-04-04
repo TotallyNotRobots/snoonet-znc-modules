721e5d7a0ec74adf9271ae01991fe3886043646f
linuxdaemon <walter@walterbarnes.net>
2017-04-04 16:32:00 -0500

    Add twitch.py

---
+++ python/twitch.py
@@ -0,0 +1,12 @@
+import znc
+
+class twitch(znc.Module):
+    description = "Adds support for Twitch.tv's custom ircv3 CAPs"
+    module_types = [znc.CModInfo.NetworkModule]
+
+    def __init__(self):
+        self.caps = ["twitch.tv/membership"]
+
+    def OnServerCapAvailable(self, cap):
+        return cap.s.lower() in self.caps
+
