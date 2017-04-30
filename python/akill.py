e2b09a8cd457130def4599d82332790e9c3441d2
linuxdaemon <walter@walterbarnes.net>
2017-04-30 16:32:54 -0500

    Cleanup akill.py and abstract some of the repeated code to module methods

--- python/akill.py
+++ python/akill.py
@@ -1,5 +1,6 @@
 import znc
 
+
 # ['akill', 'nick', 'time', 'reason', 'optional']
 #    [0]      [1]    [2]       [3]        [4]
 
@@ -7,30 +8,29 @@ import znc
 class akill(znc.Module):
     description = "Shortcut commands for akill"
     module_types = [znc.CModInfo.NetworkModule]
-
-    def __init__(self):
-        self.reasons = ["netban", "spam", "given"]
+    reasons = ["netban", "spam", "given"]
+    akill_format = "PRIVMSG OperServ :AKILL ADD +{duration} {mask} {reason}"
 
     def OnUserRaw(self, linecs):
         line = linecs.s.split()
         if line[0] == "akill":
             if len(line) < 4:
                 # too short, respond with error as a notice
-                self.PutModNotice("Usage: /akill nick/mask time(In days) reason [nick to address]")
+                self.send_usage()
                 return znc.HALT
 
             elif line[3][0] == "#" or line[3] in self.reasons:
                 if len(line) > 4:
-                    nick, time, reason, address = line[1], line[2], line[3].lower(), " ".join(line[4:])
+                    nick, time, reason, address = line[1], line[2], line[
+                        3].lower(), " ".join(line[4:])
                     if line[3][0] == "#":
                         self.evasion(time, nick, reason, address)
                     elif reason == "netban":
                         self.netban(time, nick, address)
-
                     elif reason == "spam":
                         self.spam(time, nick, address)
                     elif reason == "given":
-                        self.ownmsg(time, nick, address)
+                        self.do_akill(nick, time, address)
 
                 else:
                     nick, time, reason = line[1], line[2], line[3].lower()
@@ -41,42 +41,47 @@ class akill(znc.Module):
                     elif reason == "spam":
                         self.spam(time, nick, nick)
                     elif reason == "given":
-                        self.PutModNotice("given requires a reason to be specified")
+                        self.PutModNotice(
+                            "given requires a reason to be specified")
                     else:
-                        self.PutModNotice("Unknown option, valid options are #channel, netban, spam and given (set the "
-                                          "entire message")
+                        self.PutModNotice("Unknown option, valid options are "
+                                          "#channel, netban, spam and "
+                                          "given (set the entire message")
             else:
-                self.PutModNotice("Usage: /akill nick/mask time(In days) reason [nick to address]")
+                self.send_usage()
             return znc.HALT
 
     def evasion(self, time, nick, chan, address):
-        self.PutIRC(
-            "PRIVMSG OperServ :akill add +{time}d {nick} {address}: {time} day network ban for ban "
-            "evasion in {channel}. Further violations of our network rules will result in an increase "
-            "in ban length and may become permanent. https://snoonet.org/rules".format(
-                time=time, nick=nick, address=address, channel=chan
-            )
-        )
+        reason = "{address}: {time} day network ban for ban evasion in " \
+                 "{channel}. Further violations of our network rules will " \
+                 "result in an increase in ban length and may become " \
+                 "permanent. https://snoonet.org/rules".format(address=address,
+                                                               time=time,
+                                                               channel=chan)
+        self.do_akill(nick, time, reason)
 
     def netban(self, time, nick, address):
-        self.PutIRC(
-            "PRIVMSG OperServ :akill add +{time}d {nick} {address}: {time} day network ban for evasion "
-            "of a network ban. Further violations of our network rules will result in an increase "
-            "in ban length and may become permanent. https://snoonet.org/rules".format(
-                time=time, nick=nick, address=address
-            )
-        )
+        reason = "{address}: {time} day network ban for evasion of a " \
+                 "network ban. Further violations of our network rules will " \
+                 "result in an increase in ban length and may become " \
+                 "permanent. https://snoonet.org/rules".format(address=address,
+                                                               time=time)
+        self.do_akill(nick, time, reason)
 
     def spam(self, time, nick, address):
+        reason = "{address}: {time} day network ban for spam. Further " \
+                 "violations of our network rules will result in an " \
+                 "increase in ban length and may become permanent. " \
+                 "https://snoonet.org/rules".format(address=address, time=time)
+        self.do_akill(nick, time, reason)
+
+    def do_akill(self, nick, time, reason):
         self.PutIRC(
-            "PRIVMSG OperServ :akill add +{time}d {nick} {address}: {time} day network ban for spam."
-            " Further violations of our network rules will result in an increase "
-            "in ban length and may become permanent. https://snoonet.org/rules".format(
-                time=time, nick=nick, address=address
-            )
+            self.akill_format.format(duration=str(time) + 'd',
+                                     mask=nick,
+                                     reason=reason)
         )
 
-    def ownmsg(self, time, nick, reason):
-        self.PutIRC("PRIVMSG OperServ :akill add +{time}d {nick} {reason} ".format(
-            time=time, nick=nick, reason=reason
-        ))
+    def send_usage(self):
+        self.PutModNotice("Usage: /akill nick/mask time(In days) "
+                          "reason [nick to address]")
