97f1629767f7a15452233722d8d143fc7b80fe96
A_D <africandeath@gmail.com>
2017-06-10 17:45:52 +0200

    fixed repeated calls to len() and unneeded defaults in a .get()

--- push.py
+++ push.py
@@ -38,7 +38,6 @@ class push(znc.Module):
         sender_nick = (nick.GetNick()).lower()
         ignored = False
         highlighted = False
-
         for ignored_user in json.loads(self.nv.get('ignore', "[]")):
             if sender_nick == ignored_user:
                 ignored = True
@@ -56,6 +55,7 @@ class push(znc.Module):
                         for highlight_word in json.loads(self.nv.get('highlight', "[]")):
                             if highlight_word == word:
                                 highlighted = True
+                                break
 
             if highlighted and not ignored:
                 self.send_message(channel, nick, message)
@@ -73,7 +73,7 @@ class push(znc.Module):
             msg = "<" + nick.GetNick() + "> " + message.s
             ttl = 'Private Message'
 
-        if self.nv.get('private', "") == 'no':
+        if self.nv.get('private') == 'no':
             ttl = msg
 
         data = dict(type='note', title=ttl, body=msg)
@@ -87,12 +87,12 @@ class push(znc.Module):
         if len(command) >= 2:
             cmd_option = split_command[1]
         else:
-            cmd_option = ""
+            cmd_option = None
 
         if len(command) >= 3:
             cmd_setting = split_command[2]
         else:
-            cmd_setting = ""
+            cmd_setting = None
 
         if top_level_cmd == "enable":
             if self.nv.get('token'):
@@ -106,29 +106,28 @@ class push(znc.Module):
             self.PutModule("Notifications \x02disabled\x02.")
 
         elif top_level_cmd == "set":
-            if len(command) >= 2:
+            if cmd_option:
                 if cmd_option == "token":
 
                     if self.nv.get('state', "") == 'on':
                         self.PutModule("You must disable notifications before changing your token.")
                     else:
-                        if len(command) > 3:
-                            self.nv['token'] = command.split()[2]
+                        if cmd_option:
+                            self.nv['token'] = cmd_setting
                             self.PutModule("Token set successfully.")
 
                         else:
                             self.nv['token'] = ''
                             self.PutModule("Token cleared.")
 
-                elif cmd_option == "away_only" or cmd_option == "private":
+                elif cmd_option in ("away_only", "private"):
 
-                    if cmd_setting == "yes" or cmd_setting == "no":
+                    if cmd_setting in ("yes", "no"):
                         self.nv[cmd_option] = cmd_setting
                         self.PutModule(cmd_option + " option set to \x02" + command.split()[2] + "\x02")
 
                     else:
-                        self.PutModule(
-                            "You must specify either 'yes' or 'no'.")
+                        self.PutModule("You must specify either 'yes' or 'no'.")
 
                 else:
                     self.PutModule("Invalid option. Options are 'token' and 'away_only'. See " + help_url)
@@ -137,7 +136,7 @@ class push(znc.Module):
                 self.PutModule("You must specify a configuration option. See "
                                + help_url)
 
-        elif top_level_cmd == "highlight" or top_level_cmd == "ignore":
+        elif top_level_cmd in ("highlight", "ignore"):
 
                 if cmd_option == "list":
                     if json.loads(self.nv.get(top_level_cmd, "")):
@@ -146,7 +145,7 @@ class push(znc.Module):
                         self.PutModule(top_level_cmd.title() + " list empty.")
 
                 elif cmd_option == "add":
-                    if len(command) >= 3:
+                    if cmd_setting:
                         option_list = json.loads(self.nv.get(top_level_cmd, []))
                         if not option_list:
                             option_list = [cmd_setting.lower()]
@@ -163,7 +162,7 @@ class push(znc.Module):
                         self.PutModule("You must specify a single word or nick to add.")
 
                 elif cmd_option == "del":
-                    if len(command) >= 3:
+                    if cmd_setting:
                         if self.nv.get(top_level_cmd):
                             option_list = json.loads(self.nv[top_level_cmd])
 
@@ -178,7 +177,6 @@ class push(znc.Module):
                             self.PutModule(top_level_cmd.title() + " list empty.")
                     else:
                         self.PutModule("You must specify a single word or nick to delete.")
-
                 else:
                     self.PutModule("Invalid option. Options are 'list', "
                                    "'add', and 'del'. See " + help_url)
@@ -186,8 +184,7 @@ class push(znc.Module):
         elif top_level_cmd == "test":
             data = dict(type='note', title="Test Message",
                         body="This is a test message.")
-            requests.post('https://api.pushbullet.com/v2/pushes',
-                          auth=(self.nv['token'], ''), data=data)
+            requests.post('https://api.pushbullet.com/v2/pushes', auth=(self.nv['token'], ''), data=data)
 
             self.PutModule("Test message successfully sent.")
 
